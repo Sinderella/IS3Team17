98f6368a68eae81fc39f7813de062658c8ff4c3f
Sinderella <rawsec@gmail.com>
2014-12-02 13:42:45 +0000

    changed piechart to box chart

---
+++ js/boxchart.js
@@ -0,0 +1,54 @@
+function boxchart(yes, no) {
+    $("#boxchart").empty();
+    var width = 290;
+    var chart = d3.select("#boxchart").append("svg")
+        .attr("width", width)
+        .attr("height", 30);
+
+    var data1 = [yes];
+    var data2 = [no];
+
+    var rects = chart.selectAll(".rect1")
+        .data(data2)
+        .enter()
+        .append("rect")
+        .attr('class', 'rect1')
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("height", 30)
+        .attr("width", data1[0] * 1.0 / 100 * width)
+        .attr("fill", "#1a75ff");
+
+    var rects1 = chart.selectAll(".rect")
+        .data(data1)
+        .enter()
+        .append("rect")
+        .attr('class', 'rect')
+        .attr("x", data1[0] * 1.0 / 100 * width)
+        .attr("y", 0)
+        .attr("height", 30)
+        .attr("width", data2[0] * 1.0 / 100 * width)
+        .attr("fill", "#f71919");
+
+    var text1 = chart.selectAll(".txt1")
+        .data(data1)
+        .enter()
+        .append("text")
+        .attr("x", 7)
+        .attr("y", 18)
+        .style("fill", "#ffffff")
+        .style("font-size", 12)
+        .style("font-family", "Tahoma")
+        .text("YES " + data1[0] + "%");
+
+    var text2 = chart.selectAll(".txt2")
+        .data(data2)
+        .enter()
+        .append("text")
+        .attr("x", 153)
+        .attr("y", 18)
+        .style("fill", "#ffffff")
+        .style("font-size", 12)
+        .style("font-family", "Tahoma")
+        .text("NO " + data2[0] + "%");
+}
